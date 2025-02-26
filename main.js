e25748234667cbf6890d41029c93b5894e8c93de
leealswl <alswl54321600@gmail.com>
2025-02-27 01:59:58 +0900

    modified url

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 // API호출하고 싶을 때
-
 // const callAPI = async() =>{
 //     let url = new URL(`url주소`)
 //     let header = new Headers({헤더내용}) // 이건 필요한 경우만
@@ -28,11 +27,11 @@ function toggleSearch() {
   }
 }
 
-let API_KEY='dfa5549770ab47b7921b3ae0763768df'
+let API_KEY=''
 
 let newsList=[]
 const getLatestNews = async() =>{
-    const url =new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`)
+    const url =new URL('https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=kr&pageSize=1')
     // url = new URL(
     //     `https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=kr&pageSize=1`
     //   );
